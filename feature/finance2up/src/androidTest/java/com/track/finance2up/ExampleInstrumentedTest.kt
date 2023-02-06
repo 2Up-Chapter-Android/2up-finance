c7d2e3a37b721f58666807aadfb66a67c2ef7561
lenhathuy <huyhuy2899@gmail.com>
2023-02-06 22:13:53 +0700

    feat: [PFA-1] create codebase

---
+++ feature/finance2up/src/androidTest/java/com/track/finance2up/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.track.finance2up
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.track.finance2up.test", appContext.packageName)
+    }
+}
---
+++ feature/src/main/java/com/track/feature/MyClass.kt
@@ -0,0 +1,4 @@
+package com.track.feature
+
+class MyClass {
+}
