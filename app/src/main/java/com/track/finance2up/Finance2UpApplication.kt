f1cc7cc5e9300ff4b1435c87445524e36c6b7ad3
lenhathuy <huyhuy2899@gmail.com>
2023-02-08 10:50:38 +0700

    chore: [PFA-2] extract plant timer to function

--- app/src/main/java/com/track/finance2up/Finance2UpApplication.kt
+++ app/src/main/java/com/track/finance2up/Finance2UpApplication.kt
@@ -3,14 +3,17 @@ package com.track.finance2up
 import android.app.Application
 import timber.log.Timber
 
-class Finance2UpApplication: Application() {
+class Finance2UpApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        setupTimber()
+    }
+
+    private fun setupTimber() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
     }
-
 }
