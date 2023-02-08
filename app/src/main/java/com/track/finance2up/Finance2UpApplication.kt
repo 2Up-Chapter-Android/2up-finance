3845c7c57f2c428c16527d2571c4727b81afad3c
Le Nhat Huy <62049934+lenhathuy9281@users.noreply.github.com>
2023-02-08 10:46:42 +0700

    chore: [PFA-2] format code
    Co-authored-by: Van Linh Nguyen <33829681+linhvnguyen9@users.noreply.github.com>

--- app/src/main/java/com/track/finance2up/Finance2UpApplication.kt
+++ app/src/main/java/com/track/finance2up/Finance2UpApplication.kt
@@ -8,7 +8,7 @@ class Finance2UpApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        if (BuildConfig.DEBUG){
+        if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
     }
